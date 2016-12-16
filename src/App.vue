e1f09c5991a3fec7fea5e8485c76d718382cd256
Fritz Lin <uxfritz@163.com>
2016-12-17 01:28:10 +0800

    At.vue + App.vue

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <at :members="members">
+      <div class="editor"
+        contenteditable
+        v-html="html"></div>
+    </at>
+  </div>
+</template>
+
+<script>
+import At from './At.vue'
+
+export default {
+  components: { At },
+  name: 'app',
+  data () {
+    return {
+      members: [
+        /* eslint-disable */
+        "Roxie Miles","grace.carroll",
+        "小浩",
+        "Helena Perez","melvin.miller",
+        "椿木",
+        "myrtie.green","elsie.graham","Elva Neal",
+        "肖逵",
+        "amy.sandoval","katie.leonard","lottie.hamilton",
+        /* eslint-enable */
+      ],
+      html: `
+        <div>Awesome Electron&nbsp;<img src="static/awesome.svg"></div><div><img src="static/electron.svg"></div><div>Useful resources for creating apps with&nbsp;Electron</div><div>Inspired by the&nbsp;awesome&nbsp;list thing. You might also like&nbsp;awesome-nodejs.</div><div>Example apps</div><div>Some good apps written with Electron.</div><div>Open Source</div><div>Atom&nbsp;- Code editor.</div><div>Nuclide&nbsp;- Unified IDE.</div><div>Playback&nbsp;- Video player.</div>
+      `.trim() // fix trailing abnormal nodes
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.editor {
+  width: 400px;
+  min-height: 200px;
+  white-space: pre-wrap;
+  border: solid 2px rgba(0,0,0,.5);
+}
+.editor img {
+  max-width: 10em;
+  vertical-align: bottom;
+}
+</style>
---
+++ src/At.vue
@@ -0,0 +1,274 @@
+<style lang="scss" src="./At.scss"></style>
+
+<template>
+  <div ref="wrap"
+    class="atwho-wrap"
+    @compositionstart="handleCompositionStart"
+    @compositionend="handleCompositionEnd"
+    @input="handleInput"
+    @keydown="handleKeyDown"
+  >
+    <div v-if="atwho"
+      class="atwho-panel"
+      :style="style"
+    >
+      <div class="atwho-inner">
+        <div class="atwho-view">
+          <ul class="atwho-view-ul">
+            <li v-for="(item, index) in atwho.list"
+              :class="isCur(index) && 'cur'"
+              :ref="isCur(index) && 'cur'"
+              :data-index="index"
+              @mouseenter="handleItemHover"
+              @click="handleItemClick"
+            >
+              <span v-text="item"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+const at = '@'
+
+export default {
+  name: 'At',
+  props: {
+    members: {
+      required: true,
+      type: Array
+    }
+  },
+
+  data () {
+    return {
+      hasComposition: false,
+      atwho: null
+    }
+  },
+  computed: {
+    style () {
+      if (this.atwho) {
+        const { list, cur, x, y } = this.atwho
+        const { wrap } = this.$refs
+        if (wrap) {
+          const offset = getOffset(wrap)
+          const left = x - offset.left + 'px'
+          const top = y - offset.top + 'px'
+          return { left, top }
+        }
+      }
+      return null
+    }
+  },
+
+  methods: {
+    isCur (index) {
+      return index === this.atwho.cur
+    },
+
+    handleItemClick () {
+      this.selectItem()
+    },
+    handleItemHover (e) {
+      const el = closest(e.target, d => d.dataset.index)
+      const cur = +el.dataset.index
+      this.atwho ={
+        ...this.atwho,
+        cur
+      }
+    },
+    handleDelete (e) {
+      const range = getPrecedingRange()
+      if (range) {
+        const text = range.toString()
+        const index = text.lastIndexOf(at)
+        if (index > -1) {
+          const chunk = text.slice(index + 1, -1)
+          const { members } = this
+          const has = members.indexOf(chunk) > -1
+          if (has) {
+            e.preventDefault()
+            e.stopPropagation()
+            const r = getRange()
+            if (r) {
+              r.setStart(r.endContainer, index)
+              r.deleteContents()
+              applyRange(r)
+            }
+          }
+        }
+      }
+    },
+    handleKeyDown (e) {
+      const { atwho } = this
+      if (atwho) {
+        if (e.keyCode === 38 || e.keyCode === 40) { // ↑/↓
+          if (!(e.metaKey || e.ctrlKey)) {
+            e.preventDefault()
+            e.stopPropagation()
+            const offset = e.keyCode === 38 ? -1 : 1
+            const { members } = this
+            const { cur } = atwho
+            this.atwho = {
+              ...this.atwho,
+              cur: (cur + offset + members.length ) % members.length,
+            }
+            this.$nextTick(() => {
+              // this.$refs.cur.scrollIntoViewIfNeeded()
+              this.$refs.cur[0].scrollIntoViewIfNeeded()
+            })
+          }
+          return
+        }
+        if (e.keyCode === 13) { // enter
+          this.selectItem()
+          e.preventDefault()
+          e.stopPropagation()
+          return
+        }
+        if (e.keyCode === 27) { // esc
+          this.closePanel()
+          return
+        }
+      }
+      setTimeout(this.handleInput, 50)
+
+      if (e.keyCode === 8) {
+        this.handleDelete(e)
+      }
+    },
+
+    // compositionStart -> input -> compositionEnd
+    handleCompositionStart () {
+      this.hasComposition = true
+    },
+    handleCompositionEnd () {
+      this.hasComposition = false
+      this.handleInput()
+    },
+    handleInput () {
+      if (this.hasComposition) return
+      const range = getPrecedingRange()
+      if (range) {
+        let show = true
+        const text = range.toString()
+        const index = text.lastIndexOf(at)
+        if (index < 0) show = false
+        const prev = text[index - 1]
+
+        // 上一个字符不能为字母数字 避免与邮箱冲突
+        if (/^[a-z0-9]$/i.test(prev)) show = false
+        const chunk = text.slice(index + 1)
+        // chunk以空白字符开头不匹配 避免`@ `也匹配
+        if (/^\s/.test(chunk)) show = false
+
+        if (!show) {
+          this.closePanel()
+        } else {
+          const chunk_l = chunk.toLowerCase()
+          const { members } = this
+          const matched = members.filter(item => {
+            // match at lower-case
+            return item.toLowerCase().indexOf(chunk_l) > -1
+          })
+          if (matched.length) {
+            this.openPanel(matched, range, index)
+          } else {
+            this.closePanel()
+          }
+        }
+      }
+    },
+
+    closePanel () {
+      if (this.atwho) {
+        this.atwho = null
+      }
+    },
+    openPanel (list, range, offset) {
+      const fn = () => {
+        const r = range.cloneRange()
+        r.setStart(r.endContainer, offset + 1) // 从@后第一位开始
+        // todo: 根据窗口空间 判断向上或是向下展开
+        const rect = r.getClientRects()[0]
+        this.atwho = {
+          range,
+          offset,
+          list,
+          x: rect.left,
+          y: rect.top - 4,
+          cur: 0, // todo: 尽可能记录
+        }
+      }
+      if (this.atwho) {
+        fn()
+      } else { // 焦点超出了显示区域 需要提供延时以移动指针 再计算位置
+        setTimeout(fn, 10)
+      }
+    },
+
+    selectItem () {
+      const { range, offset, list, cur } = this.atwho
+      const r = range.cloneRange()
+      r.setStart(r.endContainer, offset + 1) // 从@后第一位开始
+      // hack: 连续两次 可以确保click后 focus回来 range真正生效
+      applyRange(r)
+      applyRange(r)
+      document.execCommand('insertText', 0, list[cur] + ' ')
+    }
+  }
+}
+
+function applyRange(range) {
+  const selection = window.getSelection()
+  if (selection) { // 容错
+    selection.removeAllRanges()
+    selection.addRange(range)
+  }
+}
+function getRange() {
+  const selection = window.getSelection()
+  if (selection && selection.rangeCount > 0) {
+    return selection.getRangeAt(0)
+  }
+}
+
+/* eslint-disable */
+// http://stackoverflow.com/questions/26747240/plain-javascript-replication-to-offset-and-position
+function getOffset(element, target) {
+    // var element = document.getElementById(element),
+    //     target  = target ? document.getElementById(target) : window;
+    target = target || window
+    var offset = {top: element.offsetTop, left: element.offsetLeft},
+        parent = element.offsetParent;
+    while (parent != null && parent != target) {
+       offset.left += parent.offsetLeft;
+       offset.top  += parent.offsetTop;
+       parent = parent.offsetParent;
+    }
+    return offset;
+}
+// http://stackoverflow.com/questions/3972014/get-caret-position-in-contenteditable-div
+function closest (el, predicate) {
+  /* eslint-disable */
+  do if (predicate(el)) return el;
+  while (el = el && el.parentNode);
+}
+// http://stackoverflow.com/questions/15157435/get-last-character-before-caret-position-in-javascript
+function getPrecedingRange() {
+  const r = getRange()
+  if (r) {
+    const range = r.cloneRange()
+    range.collapse(true)
+    var el = closest(range.endContainer, d => d.contentEditable)
+    range.setStart(el, 0)
+    return range
+  }
+}
+/* eslint-enable */
+</script>
