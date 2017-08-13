4e8e75f3be8f2ccbc202f4431cb6264c6795ae97
Fritz Lin <uxfritz@163.com>
2017-08-13 19:25:19 +0800

    feat(build): add uglifyjs to gh-pages demo

--- webpack/demo.js
+++ webpack/demo.js
@@ -3,6 +3,9 @@ var webpack = require('webpack')
 var base = require('./base')
 var config = module.exports = Object.assign({}, base)
 
+const isProd = process.env.NODE_ENV === 'production'
+console.log('isProd', isProd)
+
 Object.assign(config, {
   entry: './src/main.js',
   output: {
@@ -14,12 +17,19 @@ Object.assign(config, {
     historyApiFallback: true,
     noInfo: true
   },
-  devtool: '#eval-source-map',
+  devtool: isProd ? false : '#eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"development"'
       }
     }),
+    isProd
+      ? new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+      : { apply: () => {} }
   ]
 })
