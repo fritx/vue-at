7cdcea18613173806c1dbfd1b5ff781cc4d41add
Fritz Lin <uxfritz@163.com>
2018-04-05 00:08:31 +0800

    demo: try emojis, for #53

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,57 @@
 <template>
   <div id="app">
     <at :members="members" name-key="name" v-model="html">
-      <!-- custom: same as default slot -->
+      <!-- custom list: same as default slot -->
       <!-- <template slot="item" scope="s">
         <span v-text="s.item"></span>
       </template> -->
 
-      <!-- custom: with avatars -->
+      <!-- custom: avatars -->
       <template slot="item" scope="s">
         <img class="avatar" :src="s.item.avatar">
-        <span v-text="s.item.name"></span>
+        <span class="name" v-text="s.item.name"></span>
       </template>
 
       <div class="editor"
         contenteditable></div>
     </at>
 
-    <at :members="members" name-key="name" v-model="html2">
-      <!-- custom: same as default slot -->
-      <!-- <template slot="item" scope="s">
-        <span v-text="s.item"></span>
-      </template> -->
-
+    <at :members="emojis" name-key="name" at=":">
+        <!-- custom tag: emojis -->
       <span slot="embeddedItem" slot-scope="s">
-        <span class="tag"><img class="avatar" :src="s.current.avatar">{{ s.current.name }}</span>
+        <span v-text="s.current.icon"></span>
       </span>
 
-      <!-- custom: with avatars -->
+      <!-- custom list: emojis -->
       <template slot="item" scope="s">
-        <img :src="s.item.avatar">
-        <span v-text="s.item.name"></span>
+        <span class="icon" v-text="s.item.icon"></span>
+        <span class="name" v-text="s.item.name"></span>
       </template>
 
-      <div class="editor"
-        contenteditable></div>
+      <at :members="members" name-key="name" v-model="html2">
+        <!-- custom tag: avatars -->
+        <span slot="embeddedItem" slot-scope="s">
+          <span class="tag"><img class="avatar" :src="s.current.avatar">{{ s.current.name }}</span>
+        </span>
+
+        <!-- custom list: avatars -->
+        <template slot="item" scope="s">
+          <img class="avatar" :src="s.item.avatar">
+          <span class="name" v-text="s.item.name"></span>
+        </template>
+
+        <div class="editor"
+          contenteditable></div>
+      </at>
     </at>
 
     <br />
 
     <at-ta :members="members" name-key="name" v-model="text">
-      <!-- custom: with avatars -->
+      <!-- custom list: avatars -->
       <template slot="item" scope="s">
-        <img :src="s.item.avatar">
-        <span v-text="s.item.name"></span>
+        <img class="avatar" :src="s.item.avatar">
+        <span class="name" v-text="s.item.name"></span>
       </template>
 
       <textarea class="editor"></textarea>
@@ -75,12 +84,21 @@ members = members.map((v, i) => {
   }
 })
 
+const emojis = [
+  { icon: '👍', name: '+1' },
+  { icon: '👎', name: '-1' },
+  { icon: '😄', name: 'smile' },
+  { icon: '😃', name: 'smiley' },
+  { icon: '😅', name: 'sweat_smile' },
+]
+
 export default {
   components: { At, AtTa },
   name: 'app',
   data () {
     const data = {
       members,
+      emojis,
       text: `
 <<< Textarea >>>
 Awesome Electron 
@@ -103,8 +121,6 @@ Nuclide - Unified IDE.
 Playback - Video player.
       `.trim(), // fix trailing abnormal nodes
       html: `
-        <div>&lt;&lt;&lt; Content Editable Div &gt;&gt;&gt;</div><div>Awesome Electron&nbsp;
-        <img src="static/awesome.svg"></div><div><img style="max-width: 50px;" src="static/electron.svg"></div><div>Useful resources for creating apps with&nbsp;Electron</div><div>Inspired by the&nbsp;awesome&nbsp;list thing. You might also like&nbsp;awesome-nodejs.</div><div>Example apps</div><div>Some good apps written with Electron.</div><div>Open Source</div><div>Atom&nbsp;- Code editor.</div><div>Nuclide&nbsp;- Unified IDE.</div><div>Playback&nbsp;- Video player.</div>
         <div>&lt;&lt;&lt; Content Editable Div &gt;&gt;&gt;</div><div>Awesome Electron&nbsp;<img style="max-width: 50px;" src="static/awesome.svg"></div><div><img style="max-width: 50px;" src="static/electron.svg"></div><div>Useful resources for creating apps with&nbsp;Electron</div><div>Inspired by the&nbsp;awesome&nbsp;list thing. You might also like&nbsp;awesome-nodejs.</div><div>Example apps</div><div>Some good apps written with Electron.</div><div>Open Source</div><div>Atom&nbsp;- Code editor.</div><div>Nuclide&nbsp;- Unified IDE.</div><div>Playback&nbsp;- Video player.</div>
       `.trim() // fix trailing abnormal nodes
     }
@@ -114,7 +130,7 @@ Playback - Video player.
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -129,6 +145,8 @@ Playback - Video player.
   border: 1ps outset yellow;
 }
 .editor {
+  margin-left: 40px;
+  margin-top: 30px;
   width: 400px;
   height: 200px;
   overflow: auto;
@@ -136,7 +154,7 @@ Playback - Video player.
   border: solid 2px rgba(0,0,0,.5);
 }
 
-.avatar {
+.tag .avatar {
   max-width: 1em;
   vertical-align: middle;
 }
@@ -155,13 +173,14 @@ textarea {
   width: auto;
   -webkit-transform: scale(.8);
 }
-#app .atwho-li span {
+#app .atwho-li .icon {
+  padding-left: 4px;
+}
+#app .atwho-li .name {
   padding-left: 8px;
 }
 #app .atwho-wrap {
   display: inline-block;
   vertical-align: top;
-  margin-left: 40px;
-  margin-top: 30px;
 }
 </style>
